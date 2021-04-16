3ec4ad813cd6a6ed806c1a9534d511cb4cde592b
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-04-16 20:10:15 +0100

    feature: add battery control env

---
+++ solara/environment/battery_control.py
@@ -0,0 +1,12 @@
+"""Module with battery control environment of a photovoltaic installation."""
+
+import gym
+
+
+class BatteryControlEnv(gym.Env):
+    """A gym enviroment for controlling a battery in a PV installation."""
+
+    def __init__(self) -> None:
+        """A gym enviroment for controlling a battery in a PV installation."""
+
+        raise NotImplementedError
