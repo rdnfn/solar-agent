426ce278549e82eace2529b31b29d92e9041f478
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-04-29 16:34:21 +0100

    feature: add data based pv model

--- solara/environment/photovoltaics.py
+++ solara/environment/photovoltaics.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-class BasePV:
+class PhotovoltaicModel:
     """Base class for photovoltaic (PV) system models."""
 
     def __init__(self) -> None:
@@ -32,3 +32,58 @@ class BasePV:
             np.array: predicted generation (kW)
         """
         raise NotImplementedError
+
+
+class DataPV(PhotovoltaicModel):
+    """Photovoltaic model that samples from data."""
+
+    def __init__(
+        self, data_path: str, time_step_len: float = 1, num_steps: int = 24
+    ) -> None:
+        """Photovoltaic model that samples from data.
+
+        Args:
+            data_path (str): path to PV  data in a txt file with solar trace in kW
+            time_step_len (float): length of time steps in hours. Defaults to 1.
+            num_steps (int): number of time steps. Defaults to 24.
+        """
+        super().__init__()
+
+        self.data = np.loadtxt(data_path)
+        self.num_steps = num_steps
+        self.time_step_len = time_step_len
+
+        self.reset()
+
+    def reset(self) -> None:
+        """Reset the PV system to new randomly sampled data."""
+        self.time_step = 0
+        start = np.random.randint(len(self.data) // 24)
+        end = start + self.num_steps
+        self.episode_values = self.data[start:end]
+
+    def step(self) -> None:
+        self.time_step += 1
+
+    def get_generation(self, time: float) -> float:
+        """Get power generation for given time.
+
+        Args:
+            time (float): time of generation
+
+        Returns:
+            float: generated power (kW)
+        """
+        return self.episode_values[self.time_step]
+
+    def get_prediction(self, start_time: float, end_time: float) -> np.array:
+        """Get prediction of future PV generation.
+
+        Args:
+            start_time (float): begin of prediction
+            end_time (float): end of prediction
+
+        Returns:
+            np.array: predicted generation (kW)
+        """
+        return self.episode_values[start_time:end_time]
