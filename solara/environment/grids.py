180a825d51c17f81d893733111423484ab0336b9
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-04-30 10:57:52 +0100

    feature: add peak pricing grid model

--- solara/environment/grids.py
+++ solara/environment/grids.py
@@ -1,14 +1,10 @@
 """This module contains electrical grid models."""
 
 
-class BaseGridModel:
+class GridModel:
     """Base class for grid models."""
 
-    def __init__(self) -> None:
-        """Base class for grid models."""
-        raise NotImplementedError
-
-    def transfer(self, power: float, time: float) -> float:
+    def transfer(self, power: float) -> float:
         """Transfer power to the grid.
 
         Returns the price paid to or from home owner for either receiving
@@ -16,7 +12,6 @@ class BaseGridModel:
 
         Args:
             power (float): power to transfer (kW)
-            time (float): time of transfer
 
         Returns:
             float: price paid (can be positive or negative)
@@ -44,3 +39,52 @@ class BaseGridModel:
             float: price paid
         """
         raise NotImplementedError
+
+
+class PeakGrid(GridModel):
+    """Grid model with peak-demand pricing."""
+
+    def __init__(
+        self,
+        peak_threshold: float = 1.6,
+        base_price: float = 0.14,
+        peak_price: float = 1.0,
+        time_step_len: float = 1.0,
+    ) -> None:
+        """Grid model with peak-demand pricing.
+
+        Args:
+            peak_threshold (float): amount of power drawn above which the higher
+                peak price is charged (in kW).
+            base_price (float): price charged normally ($/kWh).
+            peak_price (float): price charged if peak threshold is passed ($/kWh).
+            time_step_len (float): duration of time step (in h).
+        """
+        self.peak_threshold = peak_threshold
+        self.peak_price = peak_price
+        self.base_price = base_price
+        self.time_step_len = time_step_len
+
+    def draw_power(self, power: float) -> float:
+        """Transfer power to the grid.
+
+        Returns the price paid to or from home owner for either receiving
+        or giving power to the grid at time t.
+
+        Args:
+            power (float): power to transfer (kW)
+            time (float): time of transfer
+
+        Returns:
+            float: price paid (can be positive or negative)
+        """
+        if power < 0:
+            raise ValueError("Peak grid model can't accept incoming power (power<0).")
+
+        if power > self.peak_threshold:
+            return (
+                self.peak_threshold * self.base_price
+                + (power - self.peak_threshold) * self.peak_price
+            ) * self.time_step_len
+        else:
+            return power * self.base_price * self.time_step_len
