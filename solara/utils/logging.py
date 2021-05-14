44349d80aa69ea7a6548198956e82317f61d6ab3
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-05-14 19:34:50 +0100

    aux: change print options for logging

--- solara/utils/logging.py
+++ solara/utils/logging.py
@@ -4,6 +4,12 @@ import logging
 
 import IPython.display
 import ipywidgets as widgets
+import numpy as np
+
+
+def setup_log_print_options():
+    """Set up print options for logging output."""
+    np.set_printoptions(precision=3, suppress=True)
 
 
 # Logging widgets
