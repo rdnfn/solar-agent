c4ed72039ca58c4b7d856597a4b13f2c97353956
rdnfn <75615911+rdnfn@users.noreply.github.com>
2021-06-07 13:52:19 +0100

    feature: add creator module that enables env config dicts

---
+++ solara/envs/creator.py
@@ -0,0 +1,46 @@
+"""Module with functions to create envs from configs."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+import solara.envs.components
+import solara.envs
+from solara.constants import DEFAULT_ENV_CONFIG
+
+if TYPE_CHECKING:
+    import gym
+
+
+def create_env(env_config: dict = None) -> gym.Env:
+    """Create a battery control environment from config.
+
+    Args:
+        env_config (Dict, optional): configuration dict for environment.
+            Defaults to None.
+
+    Returns:
+        gym.Env: environment
+    """
+
+    if env_config is None:
+        env_config = DEFAULT_ENV_CONFIG
+
+    # Creating env components (battery, solar, etc.)
+    components = {}
+    for component in env_config["components"].keys():
+
+        # Getting class
+        class_name = env_config["components"][component].pop("type")
+        component_module = getattr(solara.envs.components, component)
+        component_class = getattr(component_module, class_name)
+
+        # Creating component instance with config
+        components[component] = component_class(**env_config["components"][component])
+
+    # getting env class
+    env_module_name, env_class_name = env_config["general"].pop("type").split(".")
+    env_module = getattr(solara.envs, env_module_name)
+    env_class = getattr(env_module, env_class_name)
+
+    env = env_class(**env_config["general"], **components)
+
+    return env
